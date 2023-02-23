4b5d143c7c3e16bd8586bbb00f26fd57b050111f
Krasimir Angelov <kr.angelov@gmail.com>
2023-02-23 09:35:27 +0100

    fix typo

--- app/nlg/util.py
+++ app/nlg/util.py
@@ -80,7 +80,7 @@ class ConcrHelper:
 		"Q2381698": pgf.ExprFun("southwest_1_N"),
 		"Q6497686": pgf.ExprFun("northeast_1_N"),
 		"Q5491373": pgf.ExprFun("northwest_3_N"),
-		"Q865":     pgf.ExprFun("taiwan_2_PN")
+		"Q865":     pgf.ExprFun("taiwan_2_PN"),
 		"Q869":     pgf.ExprFun("thailand_PN")
 	}
 
