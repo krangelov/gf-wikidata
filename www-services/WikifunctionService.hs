2343a2a26f46c57ece28f5c18ca8ffda4c5c491c
Krasimir Angelov <kr.angelov@gmail.com>
2024-02-06 08:44:23 +0100

    an API for Numerals and Decimals

--- www-services/WikifunctionService.hs
+++ www-services/WikifunctionService.hs
@@ -15,6 +15,7 @@ import GF.Infra.Option
 import GF.Grammar hiding ( VRecType, VApp )
 import GF.Term
 import System.FilePath
+import Control.Applicative(liftA2)
 import Control.Monad.ST.Unsafe
 import Control.Monad(foldM)
 import PGF2
@@ -99,12 +100,19 @@ executeCode gr sgr mn qid lang code =
 
 wikiPredef :: PGF -> Map.Map Ident ([Value s] -> EvalM s (ConstValue (Value s)))
 wikiPredef pgf = Map.fromList
-  [ (identS "entity",    \[typ,VStr qid] -> fetch typ qid >>= \v -> return (Const v))
+  [ (identS "entity", \[typ,VStr qid] -> fetch typ qid >>= \v -> return (Const v))
+  , (identS "int2digits", \[VInt n] -> int2digits abstr n >>= \v -> return (Const v))
+  , (identS "int2decimal", \[VInt n] -> int2decimal abstr n >>= \v -> return (Const v))
+  , (identS "float2decimal", \[VFlt f] -> float2decimal abstr f >>= \v -> return (Const v))
+  , (identS "int2numeral", \[VInt n] -> int2numeral abstr n >>= \v -> return (Const v))
   , (identS "linearize", \[_,v] -> do let Just cnc = Map.lookup "ParseSwe" (languages pgf)
                                       e <- value2expr v
                                       return (Const (VStr (linearize cnc e))))
+  , (cLessInt,\[v1,v2] -> return (fmap toBool (liftA2 (<) (value2int v1) (value2int v2))))
   ]
   where
+    abstr = moduleNameS (abstractName pgf)
+
     fetch typ qid = do
       rsp <- unsafeIOToEvalM (simpleHTTP (getRequest ("https://www.wikidata.org/wiki/Special:EntityData/"++qid++".json")))
       case decode (rspBody rsp) >>= valFromObj "entities" >>= valFromObj qid >>= valFromObj "claims" of
@@ -167,14 +175,14 @@ matchTypeFromJSON _ _ _ = Error "Error"
 
 getFieldFromString dv id
   | id == cStr = valFromObj "value" dv >>= \s -> return [assign theLinLabel (K s)]
-  | otherwise  = Error "Not a String"
+  | otherwise  = fail "Not a String"
 
 getFieldFromWikibaseEntityId dv (field@(LIdent l), t) = do
   value <- valFromObj "value" dv
   assign field
     <$> ( case (showRawIdent l, t) of
-            (l@"id",  VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else Error "Not a String"
-            (_, _)              -> Error "Not a valid field for type wikibase-entityid"
+            (l@"id",  VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else fail "Not a String"
+            (_, _)              -> Error "Not a valid wikibase-entityid field"
         )
 
 getFieldFromGlobecoordinate dv (field@(LIdent l), t) = do
@@ -186,20 +194,19 @@ getFieldFromGlobecoordinate dv (field@(LIdent l), t) = do
             (l@"precision", VApp f []) -> if f == (cPredef,cFloat) then valFromObj l value >>= (Ok . EFloat) else fail "Not a Float"
             (l@"altitude",  VApp f []) -> if f == (cPredef,cFloat) then valFromObj l value >>= (Ok . EFloat) else fail "Not a Float"
             (l@"globe",     VSort id)  -> if id == cStr            then valFromObj l value >>= (Ok . K)      else fail "Not a String"
-            (_, _)                     -> Error "Not a valid field for type globecoordinate"
+            (_, _)                     -> Error "Not a valid globecoordinates field"
         )
 
 getFieldFromQuantity dv (field@(LIdent l), t) = do
   value <- valFromObj "value" dv -- quantity
   assign field
     <$> ( case (showRawIdent l, t) of
-            (l@"amount", VApp f []) -> if f == (cPredef,cInt)
-                                        then valFromObj l value >>= decimal EInt
-                                        else if f == (cPredef,cFloat)
-                                               then valFromObj l value >>= decimal EFloat
-                                               else fail "Not an Int or Float"
-            (l@"unit", VSort id)    -> if id == cStr            then K . dropURL <$> valFromObj l value    else fail "Not a String"
-            (_, _)                  -> fail "Not a valid field for type quantity"
+            (l@"amount", VApp f [])
+              | f == (cPredef,cInt)   -> valFromObj l value >>= decimal EInt
+              | f == (cPredef,cFloat) -> valFromObj l value >>= decimal EFloat
+              | otherwise             -> fail "Not an Int or Float"
+            (l@"unit", VSort id)      -> if id == cStr            then K . dropURL <$> valFromObj l value    else fail "Not a String"
+            (_, _)                    -> fail "Not a valid quantity field"
         )
 
 getFieldFromTime dv (field@(LIdent l), t) = do
@@ -209,16 +216,16 @@ getFieldFromTime dv (field@(LIdent l), t) = do
             (l@"time",          VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else fail "Not a String"
             (l@"precision",     VSort id) -> if id == cInt then valFromObj l value >>= decimal EInt else fail "Not an Int"
             (l@"calendarmodel", VSort id) -> if id == cStr then K . dropURL <$> valFromObj l value else fail "Not a String"
-            (_, _)                        -> fail "Not a valid field"
+            (_, _)                        -> fail "Not a valid time field"
         )
 
 getFieldFromText dv (field@(LIdent l), t) = do
   value <- valFromObj "value" dv -- monolingualtext
   assign field
     <$> ( case (showRawIdent l, t) of
-            (l@"text",     VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else Error "Not a String"
-            (l@"language", VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else Error "Not a String"
-            (_, _)                   -> Error "Not a valid field"
+            (l@"text",     VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else fail "Not a String"
+            (l@"language", VSort id) -> if id == cStr then valFromObj l value >>= (Ok . K) else fail "Not a String"
+            (_, _)                   -> fail "Not a valid text field"
         )
 
 dropURL s = match "http://www.wikidata.org/entity/" s
@@ -233,3 +240,131 @@ decimal c s =
   case reads s of
     [(v, "")] -> return (c v)
     _         -> fail "Not a decimal"
+
+int2digits abstr n
+  | n >= 0    = digits n
+  | otherwise = evalError (text "Cant convert" <+> integer n)
+  where
+    idig    = (abstr,identS "IDig")
+    iidig   = (abstr,identS "IIDig")
+
+    digit n = (VApp (abstr,identS ('D':'_':show n)) [])
+
+    digits n = do
+      let (n2,n1) = divMod n 10
+      tnk <- newEvaluatedThunk (digit n1)
+      rest n2 (VApp idig [tnk])
+
+    rest 0 t = return t
+    rest n t = do
+      let (n2,n1) = divMod n 10
+      tnk1 <- newEvaluatedThunk (digit n1)
+      tnk2 <- newEvaluatedThunk t
+      rest n2 (VApp iidig [tnk1, tnk2])
+
+int2decimal abstr n = int2digits abstr (abs n) >>= sign n
+  where
+    neg_dec = (abstr,identS "NegDecimal")
+    pos_dec = (abstr,identS "PosDecimal")
+
+    sign n t = do
+      tnk <- newEvaluatedThunk t
+      if n < 0
+        then return (VApp neg_dec [tnk])
+        else return (VApp pos_dec [tnk])
+
+float2decimal abstr f = 
+  let n = truncate f
+  in int2decimal abstr n >>= fractions (f-fromIntegral n)
+  where
+    ifrac = (abstr,identS "IFrac")
+
+    digit n = (VApp (abstr,identS ('D':'_':show n)) [])
+
+    fractions f t
+      | f < 1e-8  = return t
+      | otherwise = do
+          let f10 = f * 10
+              n2  = truncate f10
+          tnk1 <- newEvaluatedThunk t
+          tnk2 <- newEvaluatedThunk (digit n2)
+          fractions (f10-fromIntegral n2) (VApp ifrac [tnk1, tnk2])
+
+int2numeral abstr n
+  | n < 1000000000000 = n2s1000000000000 n >>= app1 "num"
+  | otherwise         = range_error n
+  where
+    n2s1000000000000 n
+      | n < 1000000000 = n2s1000000000 n >>= app1 "pot4as5"
+      | otherwise      = let (n1,n2) = divMod n 1000000000
+                         in if n2 == 0
+                            then n2s1000 n1 >>= app1 "pot5"
+                            else do n1 <- n2s1000       n1
+                                    n2 <- n2s1000000000 n2
+                                    app2 "pot5plus" n1 n2
+
+    n2s1000000000 n
+      | n < 1000000 = n2s1000000 n >>= app1 "pot3as4"
+      | otherwise   = let (n1,n2) = divMod n 1000000
+                      in if n2 == 0
+                           then n2s1000 n1 >>= app1 "pot4"
+                           else do n1 <- n2s1000    n1
+                                   n2 <- n2s1000000 n2
+                                   app2 "pot4plus" n1 n2
+
+    n2s1000000 n
+      | n < 1000  = n2s1000 n >>= app1 "pot2as3"
+      | otherwise = let (n1,n2) = divMod n 1000
+                    in if n2 == 0
+                         then n2s1000 n1 >>= app1 "pot3"
+                         else do n1 <- n2s1000 n1
+                                 n2 <- n2s1000 n2
+                                 app2 "pot3plus" n1 n2
+
+    n2s1000 n
+      | n < 100   = n2s100 n >>= app1 "pot1as2"
+      | otherwise = let (n1,n2) = divMod n 100
+                    in if n2 == 0
+                         then n2s10 n1 >>= app1 "pot2"
+                         else do n1 <- n2s10  n1
+                                 n2 <- n2s100 n2
+                                 app2 "pot2plus" n1 n2
+
+    n2s100 n
+      | n <  10   = n2s10 n >>= app1 "pot0as1"
+      | n == 10   = return (VApp (abstr,identS "pot110") [])
+      | n == 11   = return (VApp (abstr,identS "pot111") [])
+      | n <  20   = n2d (n-10) >>= app1 "pot1to19"
+      | otherwise = let (n1,n2) = divMod n 10
+                    in if n2 == 0
+                         then n2d n1 >>= app1 "pot1"
+                         else do n1 <- n2d   n1
+                                 n2 <- n2s10 n2
+                                 app2 "pot1plus" n1 n2
+
+    n2s10 n
+      | n < 1     = range_error n
+      | n == 1    = return (VApp (abstr,identS "pot01") [])
+      | otherwise = n2d n >>= app1 "pot0"
+
+    n2d n = app0 ('n':show n)
+
+    range_error n = evalError (integer n <+> text "cannot be represented as a numeral")
+
+    app0 fn = return (VApp (abstr,identS fn) [])
+
+    app1 fn v1 = do
+      tnk1 <- newEvaluatedThunk v1
+      return (VApp (abstr,identS fn) [tnk1])
+
+    app2 fn v1 v2 = do
+      tnk1 <- newEvaluatedThunk v1
+      tnk2 <- newEvaluatedThunk v2
+      return (VApp (abstr,identS fn) [tnk1,tnk2])
+
+
+value2int (VInt n) = Const n
+value2int _        = RunTime
+
+toBool True  = VApp (cPredef,identS "True")  []
+toBool False = VApp (cPredef,identS "False") []
